8a008d13267bf2b47c6ec78f68eef85e55671197
Dimitar.B.Dimitrov <dimitar.b.dimitrov@stanga.net>
2016-12-29 17:52:59 +0200

    Add authorize middleware for routs requiring logged user

--- server/config/route.config.js
+++ server/config/route.config.js
@@ -3,6 +3,12 @@
 const controllers = require('../controllers/base.controller');
 const csrf = require('csurf') ({ cookie: true });
 
+const authorize = (req, res) => {
+    if (!req.user) {
+        res.redirect('/');
+    }
+}
+
 module.exports = (app) => {
     app
         .get('/', controllers.home.index)
@@ -12,8 +18,8 @@ module.exports = (app) => {
         .get('/user/login', controllers.user.login)
         .post('/user/login', controllers.user.authenticate)
         .post('/user/logout', controllers.user.logout)
-        .get('/user/profile', csrf, controllers.user.showProfile)
-        .post('/user/profile', csrf, controllers.user.updateProfile)
+        .get('/user/profile', csrf, authorize, controllers.user.showProfile)
+        .post('/user/profile', csrf, authorize, controllers.user.updateProfile)
         .get('/error', function(req, res) {
             res.render('partial/404', {
                 message: "The page was not found",
