6e1da70650dad055ddf769935651bb5cd4319348
Dimitar.B.Dimitrov <dimitar.b.dimitrov@stanga.net>
2016-12-29 17:30:24 +0200

    Add new messages

--- server/utilities/messages-constants.js
+++ server/utilities/messages-constants.js
@@ -2,9 +2,15 @@
 
 module.exports = {
     error: {
-        inUse: "{0} {1} is already in use",
+        fieldsRequired: "Please fill out all required fields",
         incorrectInput: "{0} is not correct",
-        requiredProp: "{0} is required"
+        invalidCredentials: "Invalid password or username",
+        internal: "Server Internal Error: 500",
+        inUse: "{0} {1} is already in use",
+        requiredProp: "{0} is required",
+        minLength: "{0} must be atleast {1} characters long",
+        notMatch: "{0} does not match {1}",
+        whiteSpace: "{0} cannot be whitspace"
     },
     generateMessage: (messageType, args) => {
         let messageToReturn = messageType;
