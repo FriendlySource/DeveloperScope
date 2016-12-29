b265cfa19093653d826a2ead01176faeb29213b1
Dimitar.B.Dimitrov <dimitar.b.dimitrov@stanga.net>
2016-12-29 18:13:58 +0200

    add isInRole middleware and rename authorize to isAuthenticated, provided fix for isAuthenticated

--- server/config/route.config.js
+++ server/config/route.config.js
@@ -3,23 +3,35 @@
 const controllers = require('../controllers/base.controller');
 const csrf = require('csurf') ({ cookie: true });
 
-const authorize = (req, res) => {
-    if (!req.user) {
+const isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
         res.redirect('/');
+    }
+}
+
+const isInRole = (role) => {
+    return (req, res, next) => {
+        if (req.isAuthenticated() && req.user.role.indexof(role) > -1) {
+            next();
+        } else {
+            res.redirect('/');
+        }
     }
 }
 
 module.exports = (app) => {
     app
-        .get('/', controllers.home.index)
+        .get('/',  controllers.home.index)
         .get('/about', controllers.home.about)
         .get('/user/register', controllers.user.register)
         .post('/user/register', controllers.user.create)
         .get('/user/login', controllers.user.login)
         .post('/user/login', controllers.user.authenticate)
-        .post('/user/logout', controllers.user.logout)
-        .get('/user/profile', csrf, authorize, controllers.user.showProfile)
-        .post('/user/profile', csrf, authorize, controllers.user.updateProfile)
+        .post('/user/logout', isAuthenticated, controllers.user.logout)
+        .get('/user/profile', csrf, isAuthenticated, controllers.user.showProfile)
+        .post('/user/profile', csrf, isAuthenticated, controllers.user.updateProfile)
         .get('/error', function(req, res) {
             res.render('partial/404', {
                 message: "The page was not found",
