6ecf9ab3d24788169b6f1bc6a71512e3c5e3ad73
Dimitar.B.Dimitrov <dimitar.b.dimitrov@stanga.net>
2016-12-29 15:16:09 +0200

    fix validations

--- server/utilities/validation.js
+++ server/utilities/validation.js
@@ -4,14 +4,14 @@ module.exports = {
             return password == confirmPassword;
         },
         isWhitespace: (password) => {
-            if (!password) {
-                return false;
+            if (!password || password === '') {
+                return true;
             }
 
-            return password.trim().length > 0;
+            return password.trim().length < 1;
         },
         willPassRequiredLength: function (password, requiredLength) {
-            if (this.isWhitespace(password) || password.length !== requiredLength) {
+            if (this.isWhitespace(password) || password.length < requiredLength) {
                 return false;
             }
 
