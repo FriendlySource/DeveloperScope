32e8553752aad883399bf1d6ac4304e4ea433f27
Dimitar.B.Dimitrov <dimitar.b.dimitrov@stanga.net>
2017-01-03 10:28:52 +0200

    Add Bio and Portfolio

--- server/controllers/base.controller.js
+++ server/controllers/base.controller.js
@@ -2,8 +2,13 @@
 
 const homeController = require('./home.controller');
 const userController = require('./user.controller');
+const bioController = require('./bio.controller');
+const portfolioController = require('./portfolio.controller');
+
 
 module.exports = {
     home: homeController,
-    user: userController
+    user: userController,
+    bio: bioController,
+    portfolio: portfolioController
 };
