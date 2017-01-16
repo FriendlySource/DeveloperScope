abd5d7b404b8f4b97f5cc74ecbda84369b3cfafa
Dimitar.B.Dimitrov <dimitar.b.dimitrov@stanga.net>
2017-01-16 17:42:32 +0200

    Staring implementation of Home -> Top

--- server/controllers/home.controller.js
+++ server/controllers/home.controller.js
@@ -1,11 +1,16 @@
 'use strict';
 
+let User = require('../models/user.model');
 module.exports = {
     index: (req, res) => {
-        res.render('home', {
-            mainTitle: 'Home',
-            activePage: 'Home'
-        });
+        User.top(5)
+            .then(foundUsers => {
+                res.render('home', {
+                    mainTitle: 'Home',
+                    activePage: 'Home',
+                    top: foundUsers
+                });
+            });
     },
     about: (req, res) => {
         var user = req.user;
